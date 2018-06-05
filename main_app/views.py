1cba1d75f59dcf01de12b8921d9dc9149a53695e
graemeerickson <graemeerickson@gmail.com>
2018-06-05 14:42:10 -0700

    Defined profile view that invokes the profile template

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .models import Destination
+from django.contrib.auth.models import User
 
 def index(request):
   destinations = Destination.objects.all()
   return render(request, 'index.html', {'destinations': destinations})
+
+def profile(request, username):
+  user = User.objects.get(username=username)
+  # destinations = Destination.objects.filter(user=user)
+  return render(request, 'profile.html', {'username': username})
