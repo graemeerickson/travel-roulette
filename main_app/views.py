ee9a5ec2c23f05cd73ff59bcfe95bb879fc124fd
graemeerickson <graemeerickson@gmail.com>
2018-06-05 13:07:29 -0700

    Temporarily defined Destination class and passed sample destination cities to index template

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
+class Destination:
+  def __init__(self, city, country, region):
+    self.city = city
+    self.country = country
+    self.region = region
+
+destinations = [
+  Destination('Seattle','United States of America','North America'),
+  Destination('Barcelona','Spain','Europe'),
+  Destination('Tokyo','Japan','Asia')
+]
+
 def index(request):
-  return render(request, 'index.html')
+  return render(request, 'index.html', {'destinations': destinations})
