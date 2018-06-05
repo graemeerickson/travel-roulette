4bfa4b6450ca55dbd6f7c5fe415808f4ba54d663
graemeerickson <graemeerickson@gmail.com>
2018-06-05 14:56:25 -0700

    Implemented login functionality (url, form, view, template)

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
-  path('user/<username>/', views.profile, name='profile')
+  path('user/<username>/', views.profile, name='profile'),
+  path('login/', views.login_view, name="login"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Destination
 from django.contrib.auth.models import User
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 
 def index(request):
   destinations = Destination.objects.all()
@@ -10,3 +13,23 @@ def profile(request, username):
   user = User.objects.get(username=username)
   # destinations = Destination.objects.filter(user=user)
   return render(request, 'profile.html', {'username': username})
+
+def login_view(request):
+  if request.method == 'POST':
+    # if post, then authenticate (user submitted username and password)
+    form = LoginForm(request.POST)
+    if form.is_valid():
+      u = form.cleaned_data['username']
+      p = form.cleaned_data['password']
+      user = authenticate(username = u, password = p)
+      if user is not None:
+        if user. is_active:
+          login(request, user)
+          return HttpResponseRedirect('/')
+        else:
+          print("The account has been disabled.")
+      else:
+        print("The username and/or password is incorrect.")
+  else:
+    form = LoginForm()
+    return render(request, 'login.html', {'form': form})
