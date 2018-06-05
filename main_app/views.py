caa98b44ab532a85eb685382be71f02c68a089ad
graemeerickson <graemeerickson@gmail.com>
2018-06-05 13:18:55 -0700

    Removed Destination class in favor of storing in models

--- main_app/views.py
+++ main_app/views.py
@@ -1,16 +1,6 @@
 from django.shortcuts import render
-
-class Destination:
-  def __init__(self, city, country, region):
-    self.city = city
-    self.country = country
-    self.region = region
-
-destinations = [
-  Destination('Seattle','United States of America','North America'),
-  Destination('Barcelona','Spain','Europe'),
-  Destination('Tokyo','Japan','Asia')
-]
+from .models import Destination
 
 def index(request):
+  destinations = Destination.objects.all()
   return render(request, 'index.html', {'destinations': destinations})
