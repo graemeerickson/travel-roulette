4f8c9dd72dc941724ae05dfee86346711af9f5a4
graemeerickson <graemeerickson@gmail.com>
2018-06-05 23:44:02 -0700

    Defined destinations function that gets a random destination city from the db and passes it to the front-end.

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
-from .models import Destination
-from django.contrib.auth.models import User
 from .forms import LoginForm
+from .models import Destination
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.core import serializers
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.shortcuts import render
+import json
+import random
 
 def index(request):
   destinations = Destination.objects.all()
@@ -37,3 +40,32 @@ def login_view(request):
 def logout_view(request):
   logout(request)
   return HttpResponseRedirect('/')
+
+def destinations_view(request):
+  destinations_count = Destination.objects.count()
+  random_destination_index = random.randint(1,destinations_count)
+  random_destination = Destination.objects.get(id=random_destination_index)
+  destination = {'city': random_destination.city, 'country': random_destination.country}
+  return JsonResponse(destination)
+
+def pretty_request(request):
+  headers = ''
+  for header, value in request.META.items():
+    if not header.startswith('HTTP'):
+        continue
+    header = '-'.join([h.capitalize() for h in header[5:].lower().split('_')])
+    headers += '{}: {}\n'.format(header, value)
+
+  return (
+    '{method} HTTP/1.1\n'
+    'Content-Length: {content_length}\n'
+    'Content-Type: {content_type}\n'
+    '{headers}\n\n'
+    '{body}'
+  ).format(
+    method=request.method,
+    content_length=request.META['CONTENT_LENGTH'],
+    content_type=request.META['CONTENT_TYPE'],
+    headers=headers,
+    body=request.body,
+  )
