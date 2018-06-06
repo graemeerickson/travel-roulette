bc5b7eae7a0780758018424b755d3e6111e3bd1b
graemeerickson <graemeerickson@gmail.com>
2018-06-05 23:43:34 -0700

    Added route to destinations view

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path('user/<username>/', views.profile, name='profile'),
   path('login/', views.login_view, name="login"),
   path('logout/', views.logout_view, name="logout"),
+  path('destinations/', views.destinations_view, name="destinations"),
 ]
