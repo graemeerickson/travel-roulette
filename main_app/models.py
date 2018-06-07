70576bc817000f4d201541ad2f0a4f42e06d2468
graemeerickson <graemeerickson@gmail.com>
2018-06-07 16:52:11 -0700

    Updated Destination model with additional criteria fields - adventure, beach, ski, and months of year

--- main_app/models.py
+++ main_app/models.py
@@ -4,6 +4,21 @@ class Destination(models.Model):
   city = models.CharField(max_length=255)
   country = models.CharField(max_length=255)
   region = models.CharField(max_length=255)
+  adventure = models.BooleanField(default=False)
+  ski = models.BooleanField(default=False)
+  beach = models.BooleanField(default=False)
+  jan = models.CharField(max_length=10, default='NONE')
+  feb = models.CharField(max_length=10, default='NONE')
+  mar = models.CharField(max_length=10, default='NONE')
+  apr = models.CharField(max_length=10, default='NONE')
+  may = models.CharField(max_length=10, default='NONE')
+  jun = models.CharField(max_length=10, default='NONE')
+  jul = models.CharField(max_length=10, default='NONE')
+  aug = models.CharField(max_length=10, default='NONE')
+  sep = models.CharField(max_length=10, default='NONE')
+  oct = models.CharField(max_length=10, default='NONE')
+  nov = models.CharField(max_length=10, default='NONE')
+  dec = models.CharField(max_length=10, default='NONE')
 
   def __str__(self):
     return self.city
