f59e77a3fa3e53951d5e35bcf725079a902c804c
graemeerickson <graemeerickson@gmail.com>
2018-06-07 16:51:32 -0700

    Implemented working Signup form and Destination Search form

--- main_app/forms.py
+++ main_app/forms.py
@@ -3,3 +3,11 @@ from django import forms
 class LoginForm(forms.Form):
   username = forms.CharField(label="Username", max_length=64)
   password = forms.CharField(widget=forms.PasswordInput())
+
+class SignupForm(forms.Form):
+  username = forms.CharField(label="Username", max_length=64)
+  password = forms.CharField(widget=forms.PasswordInput())
+
+class DestinationSearchForm(forms.Form):
+  trip_purpose = forms.CharField(max_length=100)
+  selected_region = forms.CharField(max_length=100)
